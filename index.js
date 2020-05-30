9b7c5f70157aeec0e823d90144da812c9584ecfd
Sahil0906 <sahilchoudhary74306@gmail.com>
2020-05-30 16:38:56 +0530

    PORT Change

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(express.static("public"));
 // });
 
 //listen to port
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
+
 app.listen(port);
-console.log('You are listening to port 9000 : open http://localhost:9000/ in browser');
+console.log(port);
