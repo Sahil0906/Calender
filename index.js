9cfb1b9a2b3cacfc55bb5eb14826ad44da8b25ef
Sahil0906 <sahilchoudhary74306@gmail.com>
2020-05-30 15:37:39 +0530

    Variable port

--- index.js
+++ index.js
@@ -20,5 +20,6 @@ app.use(express.static("public"));
 // });
 
 //listen to port
-app.listen(9000);
+const port = process.env.port || 3000;
+app.listen(port);
 console.log('You are listening to port 9000 : open http://localhost:9000/ in browser');
